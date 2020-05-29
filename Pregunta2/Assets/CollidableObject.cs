557cfd36e49fb2fa40fbd1d0a458b72435986eff
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-05-29 03:58:29 -0500

    Pregunta 2 - Pequeñas mejoras - Limpieza de código

--- Pregunta2/Assets/CollidableObject.cs
+++ Pregunta2/Assets/CollidableObject.cs
@@ -2,73 +2,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CollidableObject : MonoBehaviour
 {
+    [Header("Collision Bounds")] [SerializeField]
+    private float width;
 
-    [SerializeField] private float width;
     [SerializeField] private float height;
 
-    [SerializeField] private GameObject elementB;
-    [SerializeField] private GameObject elementC;
+    [Header("Collisionable Objects")] [FormerlySerializedAs("elementB")] [SerializeField]
+    private GameObject rectangle_B;
+
+    [FormerlySerializedAs("elementC")] [SerializeField]
+    private GameObject rectangle_C;
 
     private bool isColliding1 = false;
     private bool isColliding2 = false;
-    
-    void Update()
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
     {
-        Collider(elementB, elementC);
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void Collider(GameObject _otherObject_1,GameObject _otherObject_2)
+    private void Update()
     {
-        if (_otherObject_1.transform.position.x < this.transform.position.x + width &&
-            _otherObject_1.transform.position.x > this.transform.position.x - width &&
-            _otherObject_1.transform.position.y < this.transform.position.y + height &&
-            _otherObject_1.transform.position.y > this.transform.position.y - height)
+        CheckCollision(rectangle_B, rectangle_C);
+        ChangeColor();
+    }
+
+    private void CheckCollision(GameObject _otherObject_1, GameObject _otherObject_2)
+    {
+        if (_otherObject_1.transform.position.x < transform.position.x + width &&
+            _otherObject_1.transform.position.x > transform.position.x - width &&
+            _otherObject_1.transform.position.y < transform.position.y + height &&
+            _otherObject_1.transform.position.y > transform.position.y - height)
         {
             isColliding1 = true;
-            print("Is Colliding: "+_otherObject_1.name);
+            print("Is Colliding: " + _otherObject_1.name);
 
-            if (_otherObject_2.transform.position.x < this.transform.position.x + width &&
-                _otherObject_2.transform.position.x > this.transform.position.x - width &&
-                _otherObject_2.transform.position.y < this.transform.position.y + height &&
-                _otherObject_2.transform.position.y > this.transform.position.y - height)
+            if (_otherObject_2.transform.position.x < transform.position.x + width &&
+                _otherObject_2.transform.position.x > transform.position.x - width &&
+                _otherObject_2.transform.position.y < transform.position.y + height &&
+                _otherObject_2.transform.position.y > transform.position.y - height)
             {
                 isColliding1 = false;
                 isColliding2 = true;
                 print("Is Double Colliding! ");
             }
-            // return true;
         }
-        else if (_otherObject_2.transform.position.x < this.transform.position.x + width &&
-                 _otherObject_2.transform.position.x > this.transform.position.x - width &&
-                 _otherObject_2.transform.position.y < this.transform.position.y + height &&
-                 _otherObject_2.transform.position.y > this.transform.position.y - height)
+        else if (_otherObject_2.transform.position.x < transform.position.x + width &&
+                 _otherObject_2.transform.position.x > transform.position.x - width &&
+                 _otherObject_2.transform.position.y < transform.position.y + height &&
+                 _otherObject_2.transform.position.y > transform.position.y - height)
         {
             isColliding1 = true;
-            print("Is Colliding: "+_otherObject_2.name);
-            // return true;
+            print("Is Colliding: " + _otherObject_2.name);
+
+            if (_otherObject_1.transform.position.x < transform.position.x + width &&
+                _otherObject_1.transform.position.x > transform.position.x - width &&
+                _otherObject_1.transform.position.y < transform.position.y + height &&
+                _otherObject_1.transform.position.y > transform.position.y - height)
+            {
+                isColliding1 = false;
+                isColliding2 = true;
+                print("Is Double Colliding! ");
+            }
         }
         else
         {
             isColliding1 = false;
             isColliding2 = false;
-            // return false;
         }
     }
 
+    public void ChangeColor()
+    {
+        if (isColliding1)
+            spriteRenderer.color = new Color(0, 1, 0, .5f);
+        else if (isColliding2)
+            spriteRenderer.color = new Color(0, 0, 1, .5f);
+        else
+            spriteRenderer.color = new Color(1, 0, 0, .5f);
+    }
+    
     private void OnDrawGizmos()
     {
-        // Gizmos.color = !isColliding1? new Color(1, 0, 0, .5f) : new Color(0, 1, 0, .5f);
         Gizmos.color = new Color(1, 0, 0, .5f);
         if (isColliding1)
             Gizmos.color = new Color(0, 1, 0, .5f);
-        else if(isColliding2)
+        else if (isColliding2)
             Gizmos.color = new Color(0, 0, 1, .5f);
-        else 
+        else
             Gizmos.color = new Color(1, 0, 0, .5f);
-        
-        Gizmos.DrawCube(this.transform.position, new Vector3(width,height,.1f));
+
+        Gizmos.DrawCube(transform.position, new Vector3(width, height, .1f));
     }
 }
