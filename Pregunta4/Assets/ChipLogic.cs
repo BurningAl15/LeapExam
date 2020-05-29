18ee5c3b4aaa494e364da55aaf3790842cb6e270
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-05-28 20:05:29 -0500

    Pregunta 4 - Interaccion Base / Inicio de logica

---
+++ Pregunta4/Assets/ChipLogic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChipLogic : MonoBehaviour
+{
+    [SerializeField] private bool chipIsColliding;
+    public enum ChipType
+    {
+        Single,Double
+    }
+
+    public ChipType chipType;
+
+    [SerializeField] private List<ColliderChecker> chipColliders = new List<ColliderChecker>();
+    
+    [SerializeField] private int index = 0;
+
+    public int Index
+    {
+        get => index;
+        set => index = value;
+    }
+
+    private bool hasChanged = false;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // if (hasChanged)
+        // {
+        //     
+        // }
+    }
+  
+}
---
+++ Pregunta4/Assets/ColliderChecker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderChecker : MonoBehaviour
+{
+    [SerializeField] private bool chipIsColliding;
+
+    public bool ChipIsColliding
+    {
+        get => chipIsColliding;
+        set => chipIsColliding = value;
+    }
+
+    [SerializeField] private ChipLogic chipLogic;
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("ChipColliding") && !chipIsColliding)
+        {
+            print("Colliding");
+            chipIsColliding = true;
+            chipLogic.Index++;
+        }
+        else
+        {
+            print("---Colliding---");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("ChipColliding") && chipIsColliding)
+        {
+            print("Not Colliding");
+            chipIsColliding = false;
+            chipLogic.Index--;
+        }
+        else
+        {
+            print("---Not Colliding---");
+        }
+    }
+}
