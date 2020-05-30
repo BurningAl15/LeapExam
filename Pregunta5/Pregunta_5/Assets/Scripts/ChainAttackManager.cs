97c1a633146bb3aad5c5d4125307d9d08b29246b
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-05-30 18:22:12 -0500

    Añadiendo proyectos de Pregunta 5 y Pregunta 6 en sus respectivas carpetas

---
+++ Pregunta5/Pregunta_5/Assets/Scripts/ChainAttackManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChainAttackManager : MonoBehaviour
+{
+    public static ChainAttackManager _instance;
+
+    [SerializeField] private int chainIndex = 0;
+
+    [SerializeField] private Text chainText;
+
+    [SerializeField] private Animator anim;
+
+    void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    public void GetDelay(float _currentDelay,float _maxDelay)
+    {
+        print("Call Delay - ChainAttackManager");
+        ChangeAlpha(_currentDelay/_maxDelay);
+    }
+
+    public void CallMessage()
+    {
+        print("Call Message - ChainAttackManager");
+        chainIndex++;
+        chainText.text = "Chain Attack!\nx" + chainIndex;
+        ChangeAlpha(1);
+        anim.SetTrigger("Hit");
+    }
+
+    public void Reset()
+    {
+        print("Call Reset - ChainAttackManager");
+        ChangeAlpha(0);
+        chainIndex = 0;
+        chainText.text = "Chain Attack!\nx" + chainIndex;
+    }
+    
+    /// <summary>
+    /// Alpha should be between 0 and 1
+    /// </summary>
+    /// <param name="_alpha"></param>
+    void ChangeAlpha(float _alpha)
+    {
+        Color initialColor = chainText.color;
+        chainText.color=new Color(initialColor.r,initialColor.g,initialColor.b,_alpha);
+    }
+}
---
+++ Pregunta6/Pregunta_6/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CameraShake : MonoBehaviour
+{
+    public static CameraShake _instance;
+
+    private Coroutine currentCoroutine = null;
+
+    
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void _Shake(float duration, float magnitude)
+    {
+        if (currentCoroutine == null)
+            currentCoroutine = StartCoroutine(Shake(duration, magnitude));
+    }
+
+    IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 orignalPosition = transform.position;
+        float elapsed = 0f;
+        
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(x, y, -10f);
+            elapsed += Time.deltaTime;
+            yield return 0;
+        }
+        transform.position = orignalPosition;
+
+        currentCoroutine = null;
+    }
+}
